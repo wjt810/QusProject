a4afa2a2a17407452e7fcbee2dbd3870beaee557
zcx <zcx525265201>
2019-03-05 15:25:02 +0800

    赵创新 -- pus_info 分页查询|添加|修改|删除  并测试

---
+++ src/main/java/springboot/service/qusinfo/QusInfoService.java
@@ -0,0 +1,21 @@
+package springboot.service.qusinfo;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import springboot.pojo.QusInfo;
+
+public interface QusInfoService {
+	//按照标题名称  模糊查询新闻资讯--分页查询
+	List<QusInfo> selectInfoByTitle(String title,Integer pageIndex,Integer pageSize);
+	//添加资讯标题
+	int addInfo(QusInfo info);
+	//根据资讯id删除资讯
+	int deleteInfoById(Integer infoId);
+	//根据id来修改资讯的信息    
+	int updateInfo(QusInfo info);//注意info  中包含要修改的infoId 和要修改的内容
+	//查询数据的总量
+	int selectCount();
+}
