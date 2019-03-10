9272c704e8ce155508e8031211998c84257cbdbf
cxy <17464@DESKTOP-KHP8R3T>
2019-03-10 23:26:19 +0800

    医生：列表信息  ------------2019年3月10日23点26分（cxy）

---
+++ src/test/java/springboot/qusdoctor/QusDoctorTest.java
@@ -0,0 +1,45 @@
+package springboot.qusdoctor;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import springboot.pojo.QusDoctor;
+import springboot.service.qusdoctor.QusDoctorService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QusDoctorTest {
+
+	@Resource
+	private QusDoctorService qusDoctorService;
+	/**
+	 * 医生列表
+	 */
+	@Test
+	public void Doctortest() {
+		List<QusDoctor> doctorList=qusDoctorService.getDoctorList();
+		System.out.println("医生姓名\t性别\t入职时间\t工龄\t角色\t挂号费用\t咨询费用\t所属科室");
+		/* // 获得今天的时间
+	    var date = new Date();
+	    startDate = new Date(startDate);
+	    var newDate = date.getTime() - startDate.getTime();*/
+		
+		for(QusDoctor doctor:doctorList) {
+			int time=new Date().getYear()-doctor.getD_born().getYear();
+			System.out.println(time);
+			System.out.println(doctor.getD_name()+"\t"+doctor.getD_sex()+"\t"+doctor.getD_startTime()
+			+"\t"+time+"\t"+doctor.getQusRole().getRole_name()+"\t"+doctor.getD_price()+"\t"+doctor.getD_consult()
+			+"\t"+doctor.getQusRoom1().getR1_name()+">"+doctor.getQusRoom2().getR2_name());
+		}
+	}
+
+}
